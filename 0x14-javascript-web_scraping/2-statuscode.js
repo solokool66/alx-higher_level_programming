474c4ed8cabaeed8b4c20a5dddea2211f4da58e6
solokool66 <solokool66@gmail.com>
2023-08-17 18:48:44 +0100

    javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2])
+
+.on('response', function (response) {
+
+	console.log(`code: ${response.statusCode}`);
+});
