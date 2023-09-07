202e59d610b81a1d7e3b9ba455a189a6e624f9d7
solokool66 <solokool66@gmail.com>
2023-09-07 18:44:43 +0100

    commited

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-def number_keys(a_dictionary):
-num = 0
-list_keys = list(a_dictionary.keys())
-for i in list_keys:
-num += 1
-return (num)
+def print_sorted_dictionary(a_dictionary):
+        """Print a dictionary by ordered keys."""
+            [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
